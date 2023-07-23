d8ad0495e55a1b3e3538f65d9fc3982661506ca2
MrUnikSurya <suryapareek491@gmail.com>
2023-07-23 14:00:23 +0530

    completed simple weather app

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Ajmer';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '68c8537bc3msh9e0d41a60adb8fcp13c722jsn3f2e328e49c7',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "68c8537bc3msh9e0d41a60adb8fcp13c722jsn3f2e328e49c7",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+const getWeather = (city) =>{
+	const url =
+	"https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city;
+		cityName.innerHTML = city
+		fetch(url, options).then(response=>response.json()).then((response) => {
+
+			console.log(response);
+			cloud_pct.innerHTML = response.cloud_pct
+			temp.innerHTML = response.temp
+			temp2.innerHTML = response.temp
+			feels_like.innerHTML = response.feels_like
+			humidity.innerHTML = response.humidity
+			humidity2.innerHTML = response.humidity
+			min_temp.innerHTML = response.min_temp
+			max_temp.innerHTML = response.max_temp
+			wind_speed.innerHTML = response.wind_speed
+			wind_speed2.innerHTML = response.wind_speed
+			wind_degrees.innerHTML = response.wind_degrees
+			sunrise.innerHTML = response.sunrise
+			sunset.innerHTML = response.sunset
+		})
+			
 }
+submit.addEventListener("click",(e)=>{
+	e.preventDefault()
+	getWeather(city.value)
+})
+getWeather("Jaipur")
